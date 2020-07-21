fb80f5f5babb16295e5af92f73df7526dc55f7ed
Azad Bolour <azadbolour@bolour.com>
2020-07-20 20:52:36 -0700

    update todo mainly

--- eagerwords-web/src/game/components/informational/EulaTextComponent.js
+++ eagerwords-web/src/game/components/informational/EulaTextComponent.js
@@ -6,7 +6,7 @@ export const EulaTextComponent = (props) => {
     <div>
       Eager Words End User Licence Agreement
 
-      Version 0.6.0 - Alpha 6. A test version. Use at your own risk.
+      Version 0.9.0 - Beta. A test version. Use at your own risk.
 
       No express or implied warrantee or suitability for any purpose.
     </div>
