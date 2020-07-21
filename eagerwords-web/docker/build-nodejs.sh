a71908c99ff58eef170a7abb3c07e49793eac51b
Azad Bolour <azadbolour@bolour.com>
2020-07-21 14:17:51 -0700

    separate out nodsjs docker image

---
+++ eagerwords-web/docker/build-nodejs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+
+namespace=$1
+tag=$2
+
+if [ -z "${namespace}" -o -z "${tag}" ]; then
+  echo "usage: $0 docker-namespace repository-tag"
+  exit 1
+fi
+
+repository=nodejs
+dockerfile=Dockerfile.${repository}
+
+docker build --no-cache --force-rm=true -f ${dockerfile} -t ${namespace}/${repository}:${tag} .
+
